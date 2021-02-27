9e32261498b39faf7c3b96f1bc31151db109f7c3
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2021-02-27 18:39:18 +0800

    Remove home-manager

--- home/kk/default.nix
+++ home/kk/default.nix
@@ -71,9 +71,6 @@ in {
 
   manual.html.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "kk";
