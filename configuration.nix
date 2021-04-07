801d54d4810d4360f8cfdd54ea80c6e9892d8cbe
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2021-04-08 00:57:37 +0800

    Updated

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.cleanTmpDir = true;
