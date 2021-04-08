20847465e111d2a6c110dbdeaeb091b4a916c828
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2021-04-09 01:55:23 +0800

    Use unstable alacritty

--- home/kk/home/alacritty.nix
+++ home/kk/home/alacritty.nix
@@ -1,10 +1,11 @@
-{ fileDir, ... }@homeAttrs:
+{ fileDir, nixpkgs-unstable, ... }@homeAttrs:
 
 { config, pkgs, ... }:
 
 let
 in {
   programs.alacritty.enable = true;
+  programs.alacritty.package = nixpkgs-unstable.alacritty;
   home.packages = with pkgs; [ hermit ];
   xdg.configFile = {
     "alacritty/alacritty.yml" = { source = "${fileDir}/.alacritty.yml"; };
