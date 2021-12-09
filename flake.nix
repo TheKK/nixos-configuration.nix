5029106e4faf648ca56b666042b3f2a76f86439e
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2021-12-09 20:28:34 +0800

    Updated

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     workaround.url = "github:NixOS/nixpkgs/edb5ff75f24e95e1ff2a05329e4c051de5eea4f2";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixos-hardware.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
--- home/kk/home/alacritty.nix
+++ home/kk/home/alacritty.nix
@@ -1,11 +1,10 @@
-{ fileDir, nixpkgs-unstable, ... }@homeAttrs:
+{ fileDir, ... }@homeAttrs:
 
 { config, pkgs, ... }:
 
 let
 in {
   programs.alacritty.enable = true;
-  programs.alacritty.package = nixpkgs-unstable.alacritty;
   home.packages = with pkgs; [ hermit ];
   xdg.configFile = {
     "alacritty/alacritty.yml" = { source = "${fileDir}/.alacritty.yml"; };
