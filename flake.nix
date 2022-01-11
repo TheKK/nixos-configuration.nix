a874b6d393640ddd70a8bc32490c573f08ae6c00
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2022-01-12 03:11:56 +0800

    Remove unused flake input, workaround

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    workaround.url = "github:NixOS/nixpkgs/edb5ff75f24e95e1ff2a05329e4c051de5eea4f2";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixos-hardware.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, workaround, nixos-hardware, home-manager }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager }: {
     # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.kks-nixos = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
